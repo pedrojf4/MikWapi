fe0e475110175b6cc0c4e6a5ab40a458e7df7957
Pedro F <pedrojf4@hotmail.com>
2025-02-09 08:56:24 -0500

    Add 4 messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import requests
 import os
 import json
@@ -7,11 +7,25 @@ load_dotenv()
 
 app = FastAPI()
 
-# URL de la API externa (cámbiala por la real)
-API_URL = "https://jsonplaceholder.typicode.com/posts/"
+
 
 URL = f'https://graph.facebook.com/{os.getenv("API_VERSION")}/{os.getenv("FROM_PHONE_NUMBER_ID")}/messages'
 HEADERS = {'Content-Type': 'application/json', 'Authorization': f'Bearer {os.getenv("ACCESS_TOKEN")}'}
+
+def send_message(message):
+    data = {
+                "messaging_product": "whatsapp",
+                "to": "3158376046",
+                "type": "text",
+                "text":{
+                "body": message
+    }
+    }
+    data = json.dumps(data)
+    response = requests.post(url=URL,headers=HEADERS, data=data)
+    print(response)
+
+
 
 @app.get("/fetch")
 def fetch_data(id: int):
@@ -21,18 +35,22 @@ def fetch_data(id: int):
     
 
     if id == 10:
+        message = "Fallo energia Mirador"
+        send_message(message)
+    elif id == 20:
+        message = "Se restablece energia Mirador"
+        send_message(message)
+    elif id == 30:
+        message = "Fallo energia Santa Barbara"
+        send_message(message)
+    elif id == 40:
+        message = "Se restablece energia Santa Barbara"
+        send_message(message)
+
+
+
     # Enviar solicitud GET a la API externa con el ID
-        data = {
-                "messaging_product": "whatsapp",
-                "to": "3158376046",
-                "type": "text",
-                "text":{
-                "body": "Evento 1 ok gachantiva Santa barbara"
-    }
-    }
-        data = json.dumps(data)
-        response = requests.post(url=URL,headers=HEADERS, data=data)
-        print(response)
+        
 
       
        
